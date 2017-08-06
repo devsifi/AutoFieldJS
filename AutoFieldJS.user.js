51571da1041d0268fd5c5f7e8287485e8c33f556
devsfiq <devsfiq@gmail.com>
2017-08-06 23:12:39 +0800

    Hopelly last time refactoring structure

--- AutoFieldJS.user.js
+++ AutoFieldJS.user.js
@@ -17,12 +17,12 @@
 
 // Value Generation
 function generateChar() {
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz';
     return chars.charAt(Math.floor(Math.random() * chars.length));
 }
 
 function generateString(length) {
-    var str = "";
+    var str = '';
     for(var i = 0; i < length; i++) {
         str += Generate.char();
     }
@@ -32,28 +32,65 @@ function generateString(length) {
 
 function generateDate(days = 0, months = 0, years = 0, format = 'DD-MM-YYYY') {
     var date = moment();
-    date = date.add(days, "d").add(months, "M").add(years, "y");
+    date = date.add(days, 'd').add(months, 'M').add(years, 'y');
 
     return date.format(format);
 }
 
 function generateTime(seconds = 0, minutes = 0, hours = 0, format = 'h:mm:ssa') {
     var date = moment();
-    date = date.add(hours, "s").add(minutes, "m").add(hours, "h");
+    date = date.add(hours, 's').add(minutes, 'm').add(hours, 'h');
 
     return date.format(format);
 }
 
-function config(name) {
-    return {
-        name    : name,
-        data    : []
-    };
+var Config = function(name, data = []) {
+    this.name = name;
+    this._data = data;
 }
 
-function configData(field, value) {
-    return {
-        field   : field,
-        value   : value
-    }
+Config.prototype.getField = function(fieldName) {
+    return _.find(this._data, function(o) {
+        return o.field == fieldName;
+    });
+}
+
+Config.prototype.fieldExists = function(fieldName) {
+    return (this.getField(fieldName) != undefined);
+}
+
+Config.prototype.addField = function(fieldName, value) {
+    this._data.push({
+        field: fieldName,
+        value: value
+    });
+}
+
+Config.prototype.removeField = function(fieldName) {
+    // var index = _.findIndex(this._data, function(o) { ;
+    //     return o.field == fieldName;
+    // });
+    // this._data = 
+
+    this._data = _.without(this._data, this.getField(fieldName));
+}
+
+var test = new Config('Test');
+console.log(test.getField('Foo'));
+console.log(test);
+test.addField('Foo', 'Bar');
+
+console.log(test);
+console.log(test.getField('Foo'));
+console.log(test.fieldExists('Foo'));
+
+test.removeField('Foo');
+
+console.log(test);
+console.log(test.getField('Foo'));
+console.log(test.fieldExists('Foo'));
+
+var PageConfig = function(url, data = []) {
+    this.url = url;
+    this._data = data;
 }
